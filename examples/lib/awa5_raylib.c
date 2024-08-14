efe3a0e28544f47637077a388ede71dfda558cc5
Raymond Li <li.raymond04@gmail.com>
2024-08-14 12:15:02 -0600

    Add raylib 3d example

--- examples/lib/awa5_raylib.c
+++ examples/lib/awa5_raylib.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+static Camera3D camera = { 0 };
+
 void initwindow(const uint8_t *data, uint8_t **out, size_t *out_len) {
     int32_t width, height;
     char title[256];
@@ -40,7 +42,7 @@ void clearbackground(const uint8_t *data, uint8_t **out, size_t *out_len) {
     memcpy(&b, data, sizeof(int32_t));
     data += sizeof(int32_t);
 
-    Color color = {r, g, b, 255};
+    Color color = { r, g, b, 255 };
 
     ClearBackground(color);
 }
@@ -70,7 +72,7 @@ void drawtext(const uint8_t *data, uint8_t **out, size_t *out_len) {
     memcpy(&b, data, sizeof(int32_t));
     data += sizeof(int32_t);
 
-    Color color = {r, g, b, 255};
+    Color color = { r, g, b, 255 };
 
     DrawText(str, posx, posy, fontsize, color);
 }
@@ -108,7 +110,155 @@ void drawcircle(const uint8_t *data, uint8_t **out, size_t *out_len) {
     memcpy(&b, data, sizeof(int32_t));
     data += sizeof(int32_t);
 
-    Color color = {r, g, b, 255};
+    Color color = { r, g, b, 255 };
 
     DrawCircle(centerx, centery, radius, color);
+}
+
+// [in] x, y, z
+void setcameraposition(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float x, y, z;
+
+    memcpy(&x, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&y, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&z, data, sizeof(float));
+    data += sizeof(float);
+
+    camera.position = (Vector3){ x, y, z };
+}
+
+// [in] x, y, z
+void setcameratarget(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float x, y, z;
+
+    memcpy(&x, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&y, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&z, data, sizeof(float));
+    data += sizeof(float);
+
+    camera.target = (Vector3){ x, y, z };
+}
+
+// [in] x, y, z
+void setcameraup(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float x, y, z;
+
+    memcpy(&x, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&y, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&z, data, sizeof(float));
+    data += sizeof(float);
+
+    camera.up = (Vector3){ x, y, z };
+}
+
+// [in] fovy
+void setcamerafovy(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float fovy;
+
+    memcpy(&fovy, data, sizeof(float));
+    data += sizeof(float);
+
+    camera.fovy = fovy;
+}
+
+// [in] projection
+void setcameraprojection(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    CameraProjection projection;
+
+    memcpy(&projection, data, sizeof(CameraProjection));
+    data += sizeof(CameraProjection);
+
+    camera.projection = projection;
+}
+
+void beginmode3d(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    BeginMode3D(camera);
+}
+
+void drawcube(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float x, y, z;
+
+    memcpy(&x, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&y, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&z, data, sizeof(float));
+    data += sizeof(float);
+
+    Vector3 position = (Vector3){ x, y, z };
+
+    float width, height, length;
+
+    memcpy(&width, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&height, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&length, data, sizeof(float));
+    data += sizeof(float);
+
+    int32_t r, g, b;
+
+    memcpy(&r, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+    memcpy(&g, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+    memcpy(&b, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+
+    Color color = { r, g, b, 255 };
+
+    DrawCube(position, width, height, length, color);
+}
+
+void drawcubewires(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    float x, y, z;
+
+    memcpy(&x, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&y, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&z, data, sizeof(float));
+    data += sizeof(float);
+
+    Vector3 position = (Vector3){ x, y, z };
+
+    float width, height, length;
+
+    memcpy(&width, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&height, data, sizeof(float));
+    data += sizeof(float);
+    memcpy(&length, data, sizeof(float));
+    data += sizeof(float);
+
+    int32_t r, g, b;
+
+    memcpy(&r, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+    memcpy(&g, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+    memcpy(&b, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+
+    Color color = { r, g, b, 255 };
+
+    DrawCubeWires(position, width, height, length, color);
+}
+
+void drawgrid(const uint8_t *data, uint8_t **out, size_t *out_len) {
+    int32_t slices;
+    float spacing;
+
+    memcpy(&slices, data, sizeof(int32_t));
+    data += sizeof(int32_t);
+    memcpy(&spacing, data, sizeof(float));
+    data += sizeof(float);
+
+    DrawGrid(slices, spacing);
 }
